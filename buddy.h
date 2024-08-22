012e84e5ce7842005bdf0892a8dc81483c9fbd1f
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-08-22 09:38:24 +0200

    minor fixes, formatting

--- buddy.h
+++ buddy.h
@@ -13,29 +13,26 @@
  *          *ptr = 'X';
  *          bfree(ptr);
  *      }
- *  buddy.h can also replace the default malloc implementation.
- *  Example:
+ *  buddy.h can also replace the default malloc implementation:
  *      $ make
  *      $ LD_PRELOAD=./libbuddy.so vim
- *
  */
 
 #ifndef BUDDY_H
 #define BUDDY_H
 
 #ifdef BUDDY_STDLIB_OVERRIDE
-    #include <stdlib.h>
-    #define BNULL    NULL
-    #define balloc   malloc
-    #define bfree    free
-    #define brealloc realloc
-    #define bcalloc  calloc
+#include <stdlib.h>
+#define BNULL    NULL
+#define balloc   malloc
+#define bfree    free
+#define brealloc realloc
+#define bcalloc  calloc
 #else
-    #define BNULL ((void *) 0)
+#define BNULL ((void *) 0)
 #endif
 
 #include <stddef.h>
-
 
 /**
  *  Allocate `size` bytes of memory.
@@ -62,8 +59,6 @@ void *bcalloc(size_t nitems, size_t size);
 
 #endif
 
-
-
 #ifdef BUDDY_IMPLEMENTATION
 #undef BUDDY_IMPLEMENTATION
 
@@ -76,19 +71,10 @@ void *bcalloc(size_t nitems, size_t size);
 typedef uint8_t byte_t;
 
 struct block {
-    size_t size;
-    int used;
-    _Alignas(max_align_t) byte_t mem[];
+	size_t size;
+	int used;
+	 _Alignas(max_align_t) byte_t mem[];
 };
-
-#ifndef BUDDY_BLOCK_INIT_SIZE
-#define BUDDY_BLOCK_INIT_SIZE 4096
-#endif
-
-_Static_assert((BUDDY_BLOCK_INIT_SIZE & (BUDDY_BLOCK_INIT_SIZE - 1)) == 0,
-               "buddy.h: BUDDY_BLOCK_INIT_SIZE must be a power of two.");
-
-
 
 // find next block
 #define NEXT(block_ptr)\
@@ -111,7 +97,6 @@ _Static_assert((BUDDY_BLOCK_INIT_SIZE & (BUDDY_BLOCK_INIT_SIZE - 1)) == 0,
 #define BYTEDIFF(ptr1, ptr2)\
     (size_t) ((byte_t *) ptr2 - (byte_t *) ptr1)
 
-
 // points to the first block in memory
 static struct block *start;
 // points to the end of last block in memory
@@ -120,7 +105,8 @@ static struct block *end;
 static struct block *next;
 // lock for the whole allocator
 static pthread_mutex_t lock;
-
+// library initialization flag
+static int Buddy_Is_Init = 0;
 
 #ifdef BUDDY_STDLIB_OVERRIDE
 // we call bfree (=free when this ^^^ is defined)
@@ -129,297 +115,271 @@ static pthread_mutex_t lock;
 #pragma GCC diagnostic ignored "-Wuse-after-free"
 #endif
 
-__attribute__((constructor))
+//__attribute__((constructor(101)))
 static void init(void)
 {
-    pthread_mutexattr_t lock_attr;
-    // lock may be aquired multiple times,
-    // for example when brealloc calls balloc
-    pthread_mutexattr_settype(&lock_attr, PTHREAD_MUTEX_RECURSIVE);
-    pthread_mutex_init(&lock, &lock_attr);
-    pthread_mutex_lock(&lock);
+	pthread_mutexattr_t lock_attr;
+	// lock may be aquired multiple times,
+	// for example when brealloc calls balloc
+	pthread_mutexattr_settype(&lock_attr, PTHREAD_MUTEX_RECURSIVE);
+	pthread_mutex_init(&lock, &lock_attr);
+	pthread_mutex_lock(&lock);
 
-    // setup an inital block of size BUDDY_BLOCK_INIT_SIZE
-    start = sbrk(0);
+	if (Buddy_Is_Init) {
+		return;
+	}
+	// setup an inital block of size BUDDY_BLOCK_INIT_SIZE
+	start = sbrk(0);
 
-    // adjust alignment if necessary
-    const size_t A = _Alignof(max_align_t);
-    if ((size_t) start % A > 0)
-    {
-        (void) sbrk(A - (size_t) start % A);
-        start = sbrk(0);
-    }
+	size_t pagesize = sysconf(_SC_PAGESIZE);
 
-    if (sbrk(BUDDY_BLOCK_INIT_SIZE) == (void *) -1)
-    {
-        assert(0 && "failed to initialize buddy.h");
-    }
-    end = (struct block *)
-        ((byte_t *) start + BUDDY_BLOCK_INIT_SIZE);
+	// adjust alignment if necessary
+	if ((size_t)start % pagesize > 0) {
+		(void)sbrk(pagesize - (size_t)start % pagesize);
+		start = sbrk(0);
+	}
 
-    next = start;
-    next->size = BUDDY_BLOCK_INIT_SIZE;
-    next->used = 0;
-    pthread_mutex_unlock(&lock);
+	if (sbrk(pagesize) == (void *)-1) {
+		assert(0 && "failed to initialize buddy.h");
+	}
+	end = (struct block *)
+	    ((byte_t *) start + pagesize);
+
+	next = start;
+	next->size = pagesize;
+	next->used = 0;
+	Buddy_Is_Init = 1;
+	pthread_mutex_unlock(&lock);
 }
 
 static struct block *grow(size_t required)
 {
-    required = BLOCKSIZE(required);
-    size_t current_size;
-    struct block *block;
+	required = BLOCKSIZE(required);
+	size_t current_size;
+	struct block *block;
 
-    // if there is just one free block,
-    // just grow that
-    if (NEXT(start) == end && !start->used)
-    {
-        size_t size = start->size;
-        while (size < required)
-        {
-            size *= 2;
-        }
+	// if there is just one free block,
+	// just grow that
+	if (NEXT(start) == end && !start->used) {
+		size_t size = start->size;
+		while (size < required) {
+			size *= 2;
+		}
 
-        if (sbrk(size - start->size) == (void *) -1)
-        {
-            return BNULL;
-        }
+		if (sbrk(size - start->size) == (void *)-1) {
+			return BNULL;
+		}
 
-        start->size = size;
-        end = NEXT(start);
+		start->size = size;
+		end = NEXT(start);
 
-        return start;
-    }
+		return start;
+	}
+	// keep growing until last block is big enough
+	do {
+		current_size = BYTEDIFF(start, end);
 
-    // keep growing until last block is big enough
-    do {
-        current_size = BYTEDIFF(start, end);
+		if (sbrk(current_size) == (void *)-1) {
+			return BNULL;
+		}
 
-        if (sbrk(current_size) == (void *) -1)
-        {
-            return BNULL;
-        }
+		block = end;
+		block->size = current_size;
+		block->used = 0;
 
-        block = end;
-        block->size = current_size;
-        block->used = 0;
+		end = NEXT(block);
 
-        end = NEXT(block);
+	} while (current_size < required);
 
-    } while (current_size < required);
-
-    return block;
+	return block;
 }
 
 static void split(struct block *block)
 {
-    block->size /= 2;
-    struct block *next = NEXT(block);
-    next->size = block->size;
-    next->used = 0;
+	block->size /= 2;
+	struct block *next = NEXT(block);
+	next->size = block->size;
+	next->used = 0;
 }
 
 static struct block *join(struct block *block)
 {
-    struct block *buddy, *joined;
-    size_t size = block->size;
+	struct block *buddy, *joined;
+	size_t size = block->size;
 
-    for (;;)
-    {
-        if (BYTEDIFF(start, block) % (size * 2) == 0)
-        {
-            buddy = (struct block *) ((byte_t *) block + size);
-            joined = block;
-        }
-        else
-        {
-            buddy = (struct block *) ((byte_t *) block - size);
-            joined = buddy;
-        }
+	for (;;) {
+		if (BYTEDIFF(start, block) % (size * 2) == 0) {
+			buddy = (struct block *)((byte_t *) block + size);
+			joined = block;
+		} else {
+			buddy = (struct block *)((byte_t *) block - size);
+			joined = buddy;
+		}
 
-        if (buddy == end ||
-            buddy->size != size ||
-            buddy->used)
-        {
-            break;
-        }
-        else
-        {
-            block = joined;
-            size *= 2;
-        }
-    }
+		if (buddy == end || buddy->size != size || buddy->used) {
+			break;
+		} else {
+			block = joined;
+			size *= 2;
+		}
+	}
 
-    block->size = size;
-    block->used = 0;
-    return block;
+	block->size = size;
+	block->used = 0;
+	return block;
 }
 
 void *balloc(size_t size)
 {
-    pthread_mutex_lock(&lock);
+	if (!Buddy_Is_Init) {
+		init();
+	}
 
-    struct block *block = next;
+	if (size == 0) {
+		return BNULL;
+	}
 
-    // search for unused block that fits allocation
-    while (MEMSIZE(block) < size || block->used)
-    {
+	pthread_mutex_lock(&lock);
 
-        block = NEXT(block);
+	struct block *block = next;
 
-        // wrap around
-        if (block == end)
-        {
-            block = start;
-        }
+	// search for unused block that fits allocation
+	while (MEMSIZE(block) < size || block->used) {
 
-        // went through all available blocks
-        // try to grow
-        if (block == next)
-        {
-            block = grow(size);
-            if (block == BNULL)
-            {
-                // can't grow
-                pthread_mutex_unlock(&lock);
-                return BNULL;
-            }
-            break;
-        }
-    }
+		block = NEXT(block);
 
-    // split until we have best fit
-    while (HALFMEMSIZE(block) >= size && block->size > MINBLOCKSIZE)
-    {
-        split(block);
-    }
+		// wrap around
+		if (block == end) {
+			block = start;
+		}
+		// went through all available blocks
+		// try to grow
+		if (block == next) {
+			block = grow(size);
+			if (block == BNULL) {
+				// can't grow
+				pthread_mutex_unlock(&lock);
+				return BNULL;
+			}
+			break;
+		}
+	}
 
-    // record where we should start searching next
-    next = NEXT(block);
-    if (next == end)
-    {
-        next = start;
-    }
+	// split until we have best fit
+	while (HALFMEMSIZE(block) >= size && block->size > MINBLOCKSIZE) {
+		split(block);
+	}
 
-    block->used = 1;
-    pthread_mutex_unlock(&lock);
-    return block->mem;
+	// record where we should start searching next
+	next = NEXT(block);
+	if (next == end) {
+		next = start;
+	}
+
+	block->used = 1;
+	pthread_mutex_unlock(&lock);
+	return block->mem;
 }
 
 void bfree(void *ptr)
 {
-    if (ptr == BNULL)
-    {
-        return;
-    }
+	if (ptr == BNULL) {
+		return;
+	}
 
-    pthread_mutex_lock(&lock);
-    struct block *block = BLOCK(ptr);
-    block = join(block);
-    next = block;
-    pthread_mutex_unlock(&lock);
+	pthread_mutex_lock(&lock);
+	struct block *block = BLOCK(ptr);
+	block = join(block);
+	next = block;
+	pthread_mutex_unlock(&lock);
 }
 
 void *brealloc(void *ptr, size_t size)
 {
-    pthread_mutex_lock(&lock);
-    struct block *block, *buddy;
-    size_t block_size;
-    byte_t *new_ptr;
+	pthread_mutex_lock(&lock);
+	struct block *block, *buddy;
+	size_t block_size;
+	byte_t *new_ptr;
 
-    if (ptr == BNULL)
-    {
-        return balloc(size);
-    }
+	if (ptr == BNULL) {
+		pthread_mutex_unlock(&lock);
+		return balloc(size);
+	}
 
-    if (size == 0)
-    {
-        bfree(ptr);
-        pthread_mutex_unlock(&lock);
-        return BNULL;
-    }
+	if (size == 0) {
+		bfree(ptr);
+		pthread_mutex_unlock(&lock);
+		return BNULL;
+	}
 
-    block = BLOCK(ptr);
+	block = BLOCK(ptr);
 
-    if (MEMSIZE(block) >= size)
-    {
-        while ((block->size / 2 - MEMOFFSET) >= size)
-        {
-            split(block);
-        }
-        next = NEXT(block);
-        pthread_mutex_unlock(&lock);
-        return block->mem;
-    }
+	if (MEMSIZE(block) >= size) {
+		while ((block->size / 2 - MEMOFFSET) >= size) {
+			split(block);
+		}
+		next = NEXT(block);
+		pthread_mutex_unlock(&lock);
+		return block->mem;
+	}
 
-    block_size = block->size;
+	block_size = block->size;
 
-    // try to grow current block by joining
-    // with only right buddies
-    for (;;)
-    {
-        if (block_size >= BLOCKSIZE(size))
-        {
-            block->size = block_size;
-            next = NEXT(block);
-            pthread_mutex_unlock(&lock);
-            return block->mem;
-        }
+	// try to grow current block by joining
+	// with only right buddies
+	for (;;) {
+		if (block_size >= BLOCKSIZE(size)) {
+			block->size = block_size;
+			next = NEXT(block);
+			pthread_mutex_unlock(&lock);
+			return block->mem;
+		}
 
-        buddy = (struct block *) ((byte_t *) block + block_size);
+		buddy = (struct block *)((byte_t *) block + block_size);
 
-        if (BYTEDIFF(start, block) % (block_size * 2) > 0 ||
-            buddy == end ||
-            buddy->size != block_size ||
-            buddy->used)
-        {
-            break;
-        }
+		if (BYTEDIFF(start, block) % (block_size * 2) > 0 ||
+		    buddy == end || buddy->size != block_size || buddy->used) {
+			break;
+		}
 
-        block_size *= 2;
-    }
+		block_size *= 2;
+	}
 
-    block_size = block->size;
-    bfree(ptr);
+	block_size = block->size;
+	bfree(ptr);
 
-    new_ptr = balloc(size);
-    if (new_ptr == BNULL)
-    {
-        // restore freed block
-        block->size = block_size;
-        block->used = 1;
-        pthread_mutex_unlock(&lock);
-        return BNULL;
-    }
+	new_ptr = balloc(size);
+	if (new_ptr == BNULL) {
+		// restore freed block
+		block->size = block_size;
+		block->used = 1;
+		pthread_mutex_unlock(&lock);
+		return BNULL;
+	}
 
-    if (new_ptr < (byte_t *)ptr)
-    {
-        for (size_t i = 0; i < MEMSIZE(block); i++)
-        {
-            new_ptr[i] = ((byte_t *)ptr)[i];
-        }
-    }
-    else
-    {
-        for (size_t i = MEMSIZE(block); i > 0; i--)
-        {
-            new_ptr[i - 1] = ((byte_t *)ptr)[i - 1];
-        }
-    }
+	if (new_ptr < (byte_t *) ptr) {
+		for (size_t i = 0; i < MEMSIZE(block); i++) {
+			new_ptr[i] = ((byte_t *) ptr)[i];
+		}
+	} else {
+		for (size_t i = MEMSIZE(block); i > 0; i--) {
+			new_ptr[i - 1] = ((byte_t *) ptr)[i - 1];
+		}
+	}
 
-    pthread_mutex_unlock(&lock);
-    return new_ptr;
+	pthread_mutex_unlock(&lock);
+	return new_ptr;
 }
 
 void *bcalloc(size_t nitems, size_t size)
 {
-    size *= nitems;
-    char *ptr = balloc(size);
-    if (ptr == BNULL)
-    {
-        return BNULL;
-    }
-    memset(ptr, 0, size);
-    return ptr;
+	size *= nitems;
+	char *ptr = balloc(size);
+	if (ptr == BNULL) {
+		return BNULL;
+	}
+	memset(ptr, 0, size);
+	return ptr;
 }
 
 #ifdef BUDDY_STDLIB_OVERRIDE
